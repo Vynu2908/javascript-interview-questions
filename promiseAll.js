9f494187f1e9932ea4374fa56f50130a7b6eaafb
Marrapu Vynatheya <vynatheya2908@gmail.com>
2025-04-30 23:20:23 +0530

    Second commit of Promise all

--- promiseAll.js
+++ promiseAll.js
@@ -1,1 +1,29 @@
-const a = 10;
+function promiseAll(promises){
+    return new Promise((resolve,reject) => {
+        const results = [];
+        let completed = 0;
+        promises.forEach((promise,index)=>{
+            Promise.resolve(promise).then(value =>{
+                results[index] = value;
+                completed++;
+
+                if(completed === promises.length){
+                    resolve(results);
+                }
+            }
+
+            ).catch(error => {
+                reject(error);
+            })
+        })
+    })
+}
+
+
+const promise1 = Promise.resolve(5);
+const promise2 = Promise.resolve(10);
+const promise3 = new Promise((resolve)=>{
+    setTimeout(()=>resolve(300),1000);
+})
+
+promiseAll([promise1, promise2, promise3]).then(result => console.log(result)).catch(error => console.log(error));
